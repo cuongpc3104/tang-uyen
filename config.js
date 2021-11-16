73aad7f6b91549598c8189c29220976876f6c9bd
cuongpc3104 <94374564+cuongpc3104@users.noreply.github.com>
2021-11-16 08:10:20 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hellooooo ! 🖐🖐🖐',
+    introDesc: `Tui có điều này muốn hỏi BU, BU nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OKiiii',
+    title: 'Uyên biết tui thích Uyên lắm hong hảaaa ? 😙',
+    desc: 'Nếu Su không trả lời hoặc thoát ra, tức là Su thích tui mà ngại hong nói đó nha :> ',
+    btnYes: 'Cóaaaa, Uyên thích Cừn nhiều lắm áaaaa 💖💖💖',
+    btnNo: '💔 Khùngggggg ! 💔 ',
+    question:'Hãy cho tui biết vì sao BU thích tui nè 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì yêu nhau từ cái nhìn đầu tiên đó Ahihihihi 😊😊',
+    mess: 'Tui biết mà, tui cũng thích Trần Dương Bảo Uyên lắm á 🥰. Lớp diu 300.000 😘😘',
+    messDesc: '💬💬 Thi ĐH xong mình tìm hiểu nhau nhá 💬💬',
+    btnAccept: 'Okiii nè 💖',
+    messLink: 'https://www.facebook.com/gg.31062/'
+}
